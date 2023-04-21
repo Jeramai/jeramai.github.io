3f3276b9a6e9fc6e8a2fead23aa5456cd96382af
Jeramai <j.faber@provrex.nl>
2023-04-21 17:57:04 +0200

    Github pages?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: isProd ? '/jeram.ai/' : '',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+
+}
