7964888361ebf73e85b89d18d98b4ba6c5359d48
Jeramai <j.faber@provrex.nl>
2023-04-21 18:28:14 +0200

    Nextconfig changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/jeram.ai/' : '',
+const nextConfig = {
   images: {
-    unoptimized: true,
+    loader: 'akamai',
+    path: '',
   },
-  reactStrictMode: true,
+  assetPrefix: './',
+};
 
-}
+export default nextConfig;
